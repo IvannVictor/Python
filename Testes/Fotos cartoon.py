cd51df06808f6853a9b0f8f994a1cd35a7ff0f7b
Ivan Victor <iivanvictor@outlook.com>
2022-05-07 20:07:00 -0300

    Update Fotos cartoon.py

--- Testes/Fotos cartoon.py
+++ Testes/Fotos cartoon.py
@@ -1,21 +1,21 @@
 from cv2 import cv2
 import numpy as np
 
-# Carregando a imagem
+# Carrega Imagem #
 img = cv2.imread(r'foto.jpg')
 
-# Passando os parametros
+# Passa parâmetros para a Lib #
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)
 edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
 
-# Cartonizando a IMG
+# Cartooniza a Imagem #
 color = cv2.bilateralFilter(img, 9, 250, 250)
 cartoon = cv2.bitwise_and(color, color, mask=edges)
 
-# Mostrar as imagens
-cv2.imshow('image', img)
-cv2.imshow('edges', edges)
-cv2.imshow('cartoon', cartoon)
+# Mostra Imagens #
+cv2.imshow("Image", img)
+cv2.imshow("edges", edges)
+cv2.imshow("Cartoon", cartoon)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
